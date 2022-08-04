231d3afdd997982722ee07fca0e69470850234e8
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2022-08-04 18:34:30 +0100

    Change to use different casing for the database to better suit postgres

--- scripts/db/domain-tables.sql
+++ scripts/db/domain-tables.sql
@@ -1,22 +1,22 @@
 create table public.games (
   id uuid not null default uuid_generate_v4(),
-  createdAt timestamptz not null default current_timestamp,
+  created_at timestamptz not null default current_timestamp,
   name varchar not null,
   stage integer not null default 0,
 
   primary key (id)
 );
 
-create table public.gameLogs (
+create table public.game_logs (
   id uuid not null default uuid_generate_v4(),
   created_at timestamptz not null default current_timestamp,
-  gameId uuid references public.games not null,
+  game_id uuid references public.games not null,
   event varchar not null,
 
   primary key (id)
 );
 
-create table public.realTeams (
+create table public.real_teams (
   id uuid not null default uuid_generate_v4(),
   name varchar not null,
 
@@ -30,13 +30,13 @@ create table public.positions (
   primary key (id)
 );
 
-create table public.realPlayers (
+create table public.real_players (
   id uuid not null default uuid_generate_v4(),
   name varchar not null,
   overall integer not null,
   potential integer not null,
-  realTeamId uuid references public.realTeams not null,
-  positionId uuid references public.positions not null,
+  real_team_id uuid references public.real_teams not null,
+  position_id uuid references public.positions not null,
   imageUrl varchar not null,
 
   primary key (id)
@@ -44,59 +44,59 @@ create table public.realPlayers (
 
 create table public.teams (
   id uuid not null default uuid_generate_v4(),
-  gameId uuid references public.games not null,
-  userId uuid references public.profiles not null,
-  teamName varchar not null,
-  managerName varchar not null,
+  game_id uuid references public.games not null,
+  user_id uuid references public.profiles not null,
+  team_name varchar not null,
+  manager_name varchar not null,
   cash integer not null,
-  isReady boolean not null default false,
-  captainBoost integer not null default 0,
-  trainingLevel integer not null default 1,
-  scoutingLevel integer not null default 1,
-  stadiumLevel integer not null default 1,
+  is_ready boolean not null default false,
+  captain_boost integer not null default 0,
+  training_level integer not null default 1,
+  scouting_level integer not null default 1,
+  stadium_level integer not null default 1,
 
   primary key (id),
-  unique (gameId, userId)
+  unique (game_id, user_id)
 );
 
-create table public.playerGameStates (
-  playerId uuid references public.realPlayers not null,
-  gameId uuid references public.games not null,
-  teamId uuid references public.teams null,
-  lineupPosition integer null,
+create table public.player_game_states (
+  player_id uuid references public.real_players not null,
+  game_id uuid references public.games not null,
+  team_id uuid references public.teams null,
+  lineup_position integer null,
   captain boolean not null default false,
   injured boolean not null default false,
   released boolean not null default false,
   stars integer not null,
 
-  primary key (playerId, gameId)
+  primary key (player_id, game_id)
 );
 
 create table public.seasons (
   id uuid not null default uuid_generate_v4(),
-  gameId uuid references public.games not null,
+  game_id uuid references public.games not null,
   name varchar not null,
-  seasoninteger integer not null,
+  season integer not null,
 
   primary key (id)
 );
 
-create table public.teamSeasons (
-  teamId uuid references public.teams not null,
-  seasonId uuid references public.seasons not null,
+create table public.team_seasons (
+  team_id uuid references public.teams not null,
+  season_id uuid references public.seasons not null,
   score integer not null,
-  startingScore integer not null,
+  starting_score integer not null,
 
-  primary key (teamId, seasonId)
+  primary key (team_id, season_id)
 );
 
 create table public.results (
   id uuid not null default uuid_generate_v4(),
-  seasonId uuid references public.seasons not null,
-  homeTeamId uuid references public.teams not null,
-  awayTeamId uuid references public.teams not null,
+  season_id uuid references public.seasons not null,
+  home_team_id uuid references public.teams not null,
+  away_team_id uuid references public.teams not null,
   draw boolean not null,
-  winningTeamId uuid references public.teams null,
+  winning_team_id uuid references public.teams null,
 
   primary key (id)
 );
